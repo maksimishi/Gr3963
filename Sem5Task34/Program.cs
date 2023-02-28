de225446314e75c1288850faeec5e848bb71bb94
maksim bykov <mak-42@list.ru>
2023-02-28 20:56:58 +0300

    Added task 30-38

---
+++ Sem5Task34/Program.cs
@@ -0,0 +1,82 @@
+﻿        const int n = 10;
+        const int minVal = 100;
+        const int maxVal = 999;
+        int[] arr = new int[n];
+        Random rand = new Random();
+
+        // Заполнение массива случайными числами
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = rand.Next(minVal, maxVal + 1);
+        }
+
+        // Вывод массива на экран
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
+
+        // Сортировка пузырьком
+        int temp;
+        bool isSorted;
+        DateTime startTime = DateTime.Now;
+        for (int i = 0; i < n - 1; i++)
+        {
+            isSorted = true;
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted)
+            {
+                break;
+            }
+        }
+        DateTime endTime = DateTime.Now;
+        TimeSpan bubbleSortTime = endTime - startTime;
+
+        // Вывод отсортированного массива на экран
+        Console.WriteLine("Массив после сортировки пузырьком:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
+
+        // Сортировка методом подсчёта
+        int[] countArr = new int[maxVal - minVal + 1];
+        startTime = DateTime.Now;
+        for (int i = 0; i < n; i++)
+        {
+            countArr[arr[i] - minVal]++;
+        }
+        int k = 0;
+        for (int i = 0; i < countArr.Length; i++)
+        {
+            for (int j = 0; j < countArr[i]; j++)
+            {
+                arr[k++] = i + minVal;
+            }
+        }
+        endTime = DateTime.Now;
+        TimeSpan countingSortTime = endTime - startTime;
+
+        // Вывод отсортированного массива на экран
+        Console.WriteLine("Массив после сортировки методом подсчёта:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
+
+        // Вывод времени сортировок
+        Console.WriteLine($"Время сортировки пузырьком: {bubbleSortTime.TotalMilliseconds} мс");
+        Console.WriteLine($"Время сортировки методом подсчёта: {countingSortTime.TotalMilliseconds} мс");
---
+++ Sem5Task38/Program.cs
@@ -0,0 +1,46 @@
+﻿
+// создаем массив из 10 вещественных чисел
+double[] array = new double[] { 3.14, 2.71, 1.618, 0.577, 1.414, 0.866, 2.718, 1.732, 1.303, 0.693 };
+
+// инициализируем переменные для хранения минимального и максимального значений
+double min = double.MaxValue;
+double max = double.MinValue;
+
+// находим минимальный и максимальный элементы массива
+foreach (double element in array)
+{
+    if (element < min)
+    {
+        min = element;
+    }
+    if (element > max)
+    {
+        max = element;
+    }
+}
+
+// находим разницу между максимальным и минимальным элементами
+double difference = max - min;
+Console.WriteLine($"Разница между максимальным и минимальным элементами: {difference}");
+
+// сортируем массив методом вставки
+for (int i = 1; i < array.Length; i++)
+{
+    double key = array[i];
+    int j = i - 1;
+
+    while (j >= 0 && array[j] > key)
+    {
+        array[j + 1] = array[j];
+        j = j - 1;
+    }
+
+    array[j + 1] = key;
+}
+
+// выводим отсортированный массив на экран
+Console.WriteLine("Отсортированный массив:");
+foreach (double element in array)
+{
+    Console.Write(element + " ");
+}
